acfa6e173d0e1423d22f64cfa33b9f65bea90662
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-04-01 20:45:44 +0200

    Process files and write to tmp dir

--- cli.js
+++ cli.js
@@ -2,14 +2,90 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 const async = require('async')
+const JSDOM = require("jsdom").JSDOM
 
 const data = yaml.load(fs.readFileSync('karte.yaml'))
-run(data)
+let svgFile
 
-function run (data) {
-  async.eachOf(data.render, (def, id, done) => render(id, def, done))
+init(err => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  run()
+})
+
+function init (callback) {
+  async.parallel([
+    done => loadSVG(done)
+  ], err => callback(err))
+}
+
+function loadSVG (callback) {
+  fs.readFile('karte.svg', (err, body) => {
+    if (err) { return callback(err) }
+
+    const dom = new JSDOM('')
+    const DOMParser = dom.window.DOMParser
+    const parser = new DOMParser
+    svgFile = parser.parseFromString(body, 'text/xml')
+    callback(null)
+  })
+}
+
+function run () {
+  async.eachOf(
+    data.render,
+    (def, id, done) => render(id, def, done),
+    (err) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+    }
+  )
 }
 
 function render (id, def, callback) {
   console.log(id, def)
+  ;(def.hide || []).forEach(labelId => {
+    const items = svgFile.querySelectorAll(convertToSelector(labelId))
+    Array.from(items).forEach(item => {
+      console.log('hide', item.getAttribute('inkscape:label'))
+      let current = item.getAttribute('style') || ''
+      item.setAttribute('style', current + ';display:none')
+    })
+  })
+
+  ;(def.show || []).forEach(labelId => {
+    const items = svgFile.querySelectorAll(convertToSelector(labelId))
+    Array.from(items).forEach(item => {
+      console.log('show', item.getAttribute('inkscape:label'))
+      let current = item.getAttribute('style') || ''
+      console.log(current)
+      current = current.split(';').filter(v => v !== 'display:none').join(';')
+
+      if (current !== '') {
+        item.setAttribute('style', current)
+      } else {
+        item.removeAttribute('style')
+      }
+    })
+  })
+
+  fs.writeFile('tmp/' + id + '.svg', svgFile.documentElement.outerHTML, (err) => callback(err))
+}
+
+function convertToSelector (str) {
+  return str
+    .split('/')
+    .map(part => {
+      if (part === '*') {
+        return '*'
+      } else {
+        return '*[inkscape:label="' + part + '"]'
+      }
+    })
+    .join(' > ')
 }
